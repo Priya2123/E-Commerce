bca881d4f9318d921d0a289ef05be6455d274f5a
Priya2123 <aggarwalpriya1121@gmail.com>
2021-04-29 22:58:27 +0530

    WIp

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,8 +3,8 @@ import {Grid} from '@material-ui/core';
 import Product from './product/Product';
 
 const productList = [
-    {id:1, name:"Shoes", description:"Running shoes", price:"$5"},
-    {id:2, name:"MacBook", description:"mac", price: "$20"},
+    {id:1, name:"Shoes", description:"Running shoes", price:"$5" ,image:'https://images.pexels.com/photos/1989836/pexels-photo-1989836.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+    {id:2, name:"MacBook", description:"mac", price: "$20", image:'https://images.pexels.com/photos/1181288/pexels-photo-1181288.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'},
 ]
 
 const Products = () => {
--- src/components/products/product/styles.js
+++ src/components/products/product/styles.js
@@ -3,5 +3,21 @@ import {makeStyles} from '@material-ui/core/styles';
 export default makeStyles(() => ({
     root: {
         maxWidth: '100%'
-    }
+    },
+    media: {
+        height: 0,
+        padding: '38%'
+    },
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+      },
+      cartActions: {
+        display: 'flex',  
+        justifyContent: 'flex-end',
+      },
+      buttons: {
+        display: 'flex',
+        alignItems: 'center',
+      },
 }))
