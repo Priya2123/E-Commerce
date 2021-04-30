d18cc0c686fd37577ccb126a752331c0389197c4
Priya2123 <aggarwalpriya1121@gmail.com>
2021-04-30 15:03:02 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Products, Navbar} from './components'; //for this - default export in index.js in components
+import {commerce} from './lib/Commerce';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+    const fetchProducts = async ()=> {
+        const {data} = await commerce.products.list();
+        setProducts(data)
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []) //run at the start on the render
+
+    // console.log(products);
+
     return (
         <div>
             <Navbar />
-            <Products/>
+            <Products products={products}/>
         </div>
     )
 }
