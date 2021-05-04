8d1afcdb118ad09219d243bcdb4989946d685685
Priya2123 <aggarwalpriya1121@gmail.com>
2021-05-04 15:24:50 +0530

    react hook form WIP

--- src/components/checkoutForm/checkout/Checkout.jsx
+++ src/components/checkoutForm/checkout/Checkout.jsx
@@ -1,45 +1,50 @@
-import React, {useState} from 'react'
-import {Paper, Stepper, Step, StepLabel, Divider, CircularProgress, Button, Typography} from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Divider,
+  CircularProgress,
+  Button,
+  Typography,
+} from '@material-ui/core'
 import useStyles from './styles'
-import AddressForm from '../AddressForm';
-import PaymentForm from '../PaymentForm';
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
 
-const steps = ["Shipping address", "Payment details"]
+const steps = ['Shipping address', 'Payment details']
 
 const Checkout = () => {
-    const classes = useStyles();
-    //0 - shipping , 1 - payment, 2 - confirmation
-    const [activeStep, setActiveStep] = useState(2)
+  const classes = useStyles()
+  //0 - shipping , 1 - payment, 2 - confirmation
+  const [activeStep, setActiveStep] = useState(0)
 
-    const Confirmation = () => (
-        <div>
-            Confirmation
-        </div>
-    )
+  const Confirmation = () => <div>Confirmation</div>
 
-    const Form = () => activeStep === 0 
-        ? <AddressForm />
-        : <PaymentForm />
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />)
 
-    return (
-        <>
-            <div className={classes.toolbar} />
-            <main className={classes.layout}>
-                <Paper className={classes.paper}>
-                    <Typography variant="h4" align="center">Checkout</Typography>
-                    <Stepper activeStep={activeStep} className={classes.stepper}>
-                        {steps.map((step) => (
-                            <Step key={step}>
-                                <StepLabel>{step}</StepLabel>
-                            </Step>
-                        ))}
-                        {/* steps.length = last step or last element of array */}
-                        {activeStep === steps.length ? <Confirmation /> : <Form/>}
-                    </Stepper>
-                </Paper>
-            </main>   
-        </>
-    )
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {/* steps.length = last step or last element of array */}
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
+  )
 }
 
 export default Checkout
